0aadd04ee66e41ea60d3e81ed2aaaa9292a6148a
Denis Hennessy <denis@peerassembly.com>
2011-03-21 01:05:57 +0000

    Implement initial converter

--- lib/csv2plist.rb
+++ lib/csv2plist.rb
@@ -1,8 +1,27 @@
+require 'fastercsv'
+require 'plist'
+
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+
 module Csv2plist
   def self.convert(src, dst, opts={})
-    puts "#{src} -> #{dst}"
+    rows = FasterCSV.read(src)
+    cols = rows[0]
+    entries = []
+    puts "Columns: #{cols.inspect}"
+    (1..rows.length-1).each do |row|
+      vals = rows[row]
+      dict = {}
+      cols.each_with_index do |c,i|
+        if vals[i]
+          value = vals[i].strip
+          dict[c] = value if c && value.length > 0
+        end
+      end
+      entries << dict
+    end
+    entries.save_plist(dst)
   end
 end
